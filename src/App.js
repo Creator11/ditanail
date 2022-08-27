fb2645213063e017b2012d64a3b15cfd7c12ead7
Danil Troshin <troshin@Air-Danil.Dlink>
2022-08-27 20:57:51 +0400

    init project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        hello this is my project
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+ 
+
+export function Main(props) {
+    return <h1>Привет, {props.name}</h1>;
+}
